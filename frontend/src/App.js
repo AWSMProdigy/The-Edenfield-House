ecf05db85af96e850713e4254354937c601892b3
AWSMProdigy <StraightAKyle@gmail.com>
2021-12-14 13:01:52 -0500

    Got page to load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Header from './components/Header/Header';
 import About from './components/About/About';
 import Home from './components/Home/Home';
@@ -6,27 +7,21 @@ import Photos from './components/Photos/Photos';
 import Footer from './components/Footer/Footer';
 import Contact from './components/Contact/Contact';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header/>
       <Router>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/About">
-          <About/>
-        </Route>
-        <Route exact path="/Photos">
-          <Photos/>
-        </Route>
-        <Route exact path="/Contact">
-          <Contact/>
-        </Route>
-      </Router>
+      <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/About" element={<About/>}/>
+        <Route exact path="/Photos" element={<Photos/>}/>
+        <Route exact path="/Contact" element={<Contact/>}/>
+      </Routes>
       <Footer/>
+      </Router>
     </>
   );
 }
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,17 @@
 import './Header.css';
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 function Header() {
     return (
-        <></>
+        <header className='headerFlex'>
+            <Link className="navTitle" to="/">The Edenfield House</Link>
+            <nav>
+                <Link to="/About">About us</Link>
+                <Link to="/Contact">Contact us</Link>
+                <Link to="/Photos">Photos</Link>
+            </nav>
+        </header>
     )
 }
 
